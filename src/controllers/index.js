5e2a444fbb32348715e61c95c8b71bfee4c986ba
blackd44 <irabd44@gmail.com>
2024-04-18 14:31:41 +0200

    add example api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,7 @@
 export const TestController = (req, res) => {
   res.json({ message: "test complete successful" });
 };
+
+export const exampleController = (req, res) => {
+  res.json({ message: "this is example api" });
+}
