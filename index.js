5860179c5c6713de0eb0ce47f6923ac0779ddbff
berithb <166835833+berithb@users.noreply.github.com>
2024-04-22 10:57:54 +0200

    Configure the app to use mysql as DB Engine and Sequelize ORM

---
+++ index.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('XM_bakeries', 'root', 
+null, {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+  
